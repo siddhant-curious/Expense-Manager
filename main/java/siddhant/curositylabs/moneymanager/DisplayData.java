67fba2428ba57f3f62f0ecaf69dab51febbceea5
Siddhant <siddhantchhabra96@gmail.com>
2016-08-11 09:51:15 +0530

    Added DisplayData (On Click List) Activity

--- main/java/siddhant/curositylabs/moneymanager/DisplayData.java
+++ main/java/siddhant/curositylabs/moneymanager/DisplayData.java
@@ -1,5 +1,7 @@
 package siddhant.curositylabs.moneymanager;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,11 +10,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class DisplayData extends AppCompatActivity implements AdapterView.OnItemClickListener {
@@ -22,10 +26,27 @@ public class DisplayData extends AppCompatActivity implements AdapterView.OnItem
     List<String> itemNamesList;
     ArrayList itemDetails;
 
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
+
+        dateView = (TextView) findViewById(R.id.editDate);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+
+
+
+
         mydb = new DatabaseHandler(this);
 
         itemNamesList = new ArrayList<String>();
@@ -88,4 +109,36 @@ public class DisplayData extends AppCompatActivity implements AdapterView.OnItem
 
         startActivity(showDetails);
     }
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
 }
